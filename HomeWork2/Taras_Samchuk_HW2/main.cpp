52fca2d0a5ec2c845ac5bfa2ee711ae93b62d5e3
Taras Samchuk (Sam4uk) <sto.ukr.net@gmail.com>
2020-05-20 23:42:24 +0300

    dev: unique

--- HomeWork2/Taras_Samchuk_HW2/main.cpp
+++ HomeWork2/Taras_Samchuk_HW2/main.cpp
@@ -1,16 +1,33 @@
 // #include "unique_ptr.hpp"
+#include <fstream>
 #include "../../HomeWork1/Taras_Samchuk_HW1/DEBUG1.code.hpp"
-#include "unique_ptr.code.hpp"
-#include <array>
+#include "unique_ptr.hpp"
 int main(/* int argc, char const *argv[] */) {
   DEBUG;
   {
-    UniquePtr<int[5]> TEST;
-    LogMessage(TEST.get(), "\n");
+    LogMessage("UniquePtr<int> ptr1{new int{}}\n");
+    UniquePtr<int> ptr1{new int{}};  // use ~Unique_ptr() {delete ptr;}
+  }
+
+  {
+    LogMessage("UniquePtr<int[]> ptr2{new int[200]{}};\n");
+    // UniquePtr<int[]> ptr2{new int[200]{}};  // use ~Unique_ptr() {delete[]
+    // ptr;}
   }
   {
-    UniquePtr<std::array<int,6>> TEST2;
-    // TEST2[3];
+    LogMessage("Unique_ptr<File> ptr{file, fclose};\n");
+    auto file = fopen("Makefile", "r");
+    // UniquePtr<std::FILE> ptr{file, fclose};  // use ~Unique_ptr()
+    // {fclose(ptr);}
   }
-  return 0;
-}
+  {
+    LogMessage("UniquePtr<int> ptr_{new int{}};\n");
+    UniquePtr<int> ptr{new int{}};
+
+    LogMessage(
+        "UniquePtr<int> ptr_copy{ptr};              // compiler error\n"
+        "UniquePtr<int> ptr_copy{std::move(ptr)};   // compiler OK}\n");
+    // UniquePtr<int> ptr_copy{ptr};             // compiler error
+    UniquePtr<int> ptr_copy{std::move(ptr)};  // compiler OK}
+    return 0;
+  }
--- HomeWork2/Taras_Samchuk_HW2/unique_ptr.hpp
+++ HomeWork2/Taras_Samchuk_HW2/unique_ptr.hpp
@@ -1,6 +1,27 @@
-
+#pragma once
 #include <iostream>
-#include "../../HomeWork1/Taras_Samchuk_HW1/DEBUG1.code.hpp"
+
+template <typename T>
+class Deleter {
+ public:
+  typedef T* pointer;
+  void operator()(pointer t) const {
+    std::cout << "()-----------------\n";
+    delete t;
+  }
+};
+template <typename T>
+class Deleter<T[]> {
+ public:
+  typedef T* pointer;
+  void operator()(pointer t) const {
+    std::cout << "()-------[]----------\n";
+    delete[] t;
+  }
+};
+
+template <typename T, typename F = Deleter<T>>
+class UniquePtr;
 
 template <typename T, typename F>
 class UniquePtr {
@@ -9,44 +30,38 @@ class UniquePtr {
 
   UniquePtr() = default;
   UniquePtr(const UniquePtr&) = delete;
-  UniquePtr(pointer);
+  UniquePtr& operator=(const UniquePtr&) = delete;
+  UniquePtr(pointer ptr) : _ptr(ptr){};
+
+  UniquePtr& operator=(pointer ptr) {
+    free();
+    _ptr = ptr;
+    return *this;
+  };
+
+  UniquePtr(UniquePtr&& ptr) : _ptr(ptr._ptr) { ptr._ptr = nullptr; }
 
   pointer release() {
-    pointer copy = _ptr;
+    pointer tmp = _ptr;
     _ptr = nullptr;
-    return copy;
+    return tmp;
   };  // Releases the ownership of the managed object
-  void reset(pointer /* ptr */ t) {
+  void reset(pointer ptr) {
     free();
-    _ptr = t;
-  };                               // Replaces the managed object
-  pointer get() { return _ptr; };  // Returns a pointer to the managed object
-  explicit operator bool() {
-    return (_ptr) ? true : false;
-  };  // like get() != nullptr
-  pointer operator->() { return &(this->operator*()); };
-  T& operator*();
-  T& operator[](size_t i);  // if Unique_ptr store array
+    _ptr = ptr;
+  };                              // Replaces the managed object
+  pointer get() { return _ptr; }  // Returns a pointer to the managed object
+  explicit operator bool() const {
+    return (_ptr != nullptr) ? true : false;
+  }  // like get() != nullptr
+  pointer operator->() const { return &(this->operator*()); }
+  T& operator*() const { return *_ptr; }
+  T& operator[](size_t index) { return &(_ptr + index); };
+  ~UniquePtr() { free(); }
+
  private:
   pointer _ptr{nullptr};
-  void free() { F(_ptr); };
+  void free() {  F(_ptr);
+    std::cout << __PRETTY_FUNCTION__ << std::endl;
+  }
 };
-
-template <typename T>
-class Deleter {
- public:
-  void operator()(T* p) { delete p; }
-};
-
-// template <typename T[]>
-// class Deleter {
-//  public:
-//   void operator()(UniquePtr<T>::pointer p) { delete[] p; }
-// };
-
-template <typename T, typename F = Deleter<T>>
-class UniquePtr;
-
-//===========================================================================
-template <typename T>
-UniquePtr::UniquePtr(poin)
