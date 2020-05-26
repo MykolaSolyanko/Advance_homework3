3637ab6f18537b395c3d0eb737e9eb2580ef1b22
Taras Samchuk (Sam4uk) <sto.ukr.net@gmail.com>
2020-05-26 12:04:14 +0300

    fix: deleting overhead in stack

--- HomeWork1/Taras_Samchuk_HW1/stek.hpp
+++ HomeWork1/Taras_Samchuk_HW1/stek.hpp
@@ -7,32 +7,19 @@
 template <class T, class Cont = Vector<T>>
 class Stack {
  public:
-  // Stack() = default;
+  Stack() = default;
   Stack(const size_t size) { proxy.reserve(size); };
   ~Stack() = default;
 
   void push(const T& value) { proxy.push_front(value); };
-  T top() {
-    if (proxy.empty()) {
-      return {};
-    }
-    return proxy.front();
-  };
+  T top() { return proxy.front(); };
   T pop() {
-    if (proxy.empty()) {
-      return {};
-    };
     T tmp = top();
     proxy.erase(proxy.end());
     return tmp;
   };
 
-  void clear() {
-    if (!proxy.empty()) {
-      return;
-    }
-    proxy.erase(proxy.begin(), proxy.end());
-  };
+  void clear() { proxy.erase(proxy.begin(), proxy.end()); };
 
   bool isEmpty() const { return proxy.empty(); };
 
