f67ea22d6ab32ef0e7ff8ad01152e58158e07770
PanMaksim <60920533+PanMaksim@users.noreply.github.com>
2020-05-28 20:25:02 +0300

    Update Maksim_Panibrat_HW1_Stack.hpp

--- HomeWork1/Maksim_Panibrat_HW1_Stack.hpp
+++ HomeWork1/Maksim_Panibrat_HW1_Stack.hpp
@@ -1,49 +1,62 @@
 #pragma once
-using pair = std::pair<bool, const char *>;
 
-template <typename T, class Vector> class Stack {
+using pair = std::pair<bool, const char*>;
+
+template <typename T, class Container>
+class Stack_ad {
 public:
-  pair push(T value) {
-    if (vec.size_() > vec.capacity_()) {
-      return {false, "Stack full"};
-    }
-    vec.push_back(value);
-    return {true, "Push success"};
-  }
-  pair pop() {
-    if (vec.size_() == 0) {
-      return {false, "Stack clear"};
-    }
-    vec.erase(vec.end());
-    return {true, "Pop success"};
-  }
 
-  void clear() {
-    if (vec.size_() == 0) {
-      return;
-    }
-    vec.clear();
-  }
+	pair push(T value) {
+		if (vec.size_() > vec.capacity_()) {
+			return { false, "Stack full" };
+		}
+		vec.push_back(value);
+		return { true, "Push success" };
+	}
+	pair pop() {
+		if (vec.size_() == 0) {
+			return { false, "Stack clear" };
+		}
+		vec.erase(vec.end());
+		return { true, "Pop success" };
+	}
 
-  std::pair<bool, T> top() {
-    // runtime error if size == 0 because of no default init for clear T element
-    if (vec.size_() == 0) {
-      return {false, vec.back()};
-    }
+	void clear() {
+		if (vec.size_() == 0) {
+			return;
+		}
+		vec.clear();
+	}
 
-    return {true, vec.back()};
-  }
+	std::pair<bool, T> top() {
+		if (vec.size_() == 0) {
+			return { false, vec.back() };
+		}
 
-template <typename... Rest> void emplace(Rest... rest) {
-  std::cout << __FUNCTION__ << std::endl;
-  vec.push_back(T{rest...});
-}
+		return { true, vec.back() };
+	}
+	
+	template <typename ... Rest>
+	void emplace(Rest... rest) {
+		std::cout << __FUNCTION__ << std::endl;
+		vec.push_back(T{rest...});
+	}
 
-  const unsigned long long GetSize() { return vec.size_(); }
-  const unsigned long long GetCapacity() { return vec.capacity_(); }
-  void analize() { vec.analize(); }
+	void resize(size_t new_size) {
+		std::cout << __FUNCTION__ << std::endl;
+		vec.resize(new_size);
+	}
+
+	const unsigned long long GetSize() {
+		return vec.size_();
+	}
+	const unsigned long long GetCapacity() {
+		return vec.capacity_();
+	}
+	void analize() {
+		vec.analize();
+	}
 
 private:
-  // vec size init to do default init for elements
-  Vector vec{4};
+	Container vec{};
 };
