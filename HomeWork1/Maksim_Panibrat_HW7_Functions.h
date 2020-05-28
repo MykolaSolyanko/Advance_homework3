7abc8498520324a2c30fbf3ba1d84632fb1e3097
PanMaksim <60920533+PanMaksim@users.noreply.github.com>
2020-05-28 20:28:04 +0300

    Create Maksim_Panibrat_HW7_Functions.h

---
+++ HomeWork1/Maksim_Panibrat_HW7_Functions.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <iostream>
+
+void guide();
+
+void check_vector_constructors();
+void check_vector_memory();
+void check_vector_operators();
+void check_vector_functions();
+void stack_adapter();
