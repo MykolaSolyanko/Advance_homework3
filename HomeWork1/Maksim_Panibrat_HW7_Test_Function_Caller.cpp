5710a68c742d06e4d193a4dceef11579626ee081
PanMaksim <60920533+PanMaksim@users.noreply.github.com>
2020-06-03 15:13:06 +0300

    Update and rename Maksim_Panibrat_HW7_Function_Caller.cpp to Maksim_Panibrat_HW7_Test_Function_Caller.cpp

--- HomeWork1/Maksim_Panibrat_HW7_Test_Function_Caller.cpp
+++ HomeWork1/Maksim_Panibrat_HW7_Test_Function_Caller.cpp
@@ -1,47 +1,57 @@
-#include "Functions_List.h"
+#include "Test_Functions_List.h"
 
-enum GUIDE {
+enum TESTER {
     CHECK_VECTOR_CONSTRUCTORS = 1,
     CHECK_VECTOR_MEMORY,
     CHECK_VECTOR_OPERATORS,
     CHECK_VECTOR_FUNCTIONS,
     STACK_ADAPTER,
-    END_GUIDE,
+    ALL_FUNCTIONS,
+    END_GUIDE
 };
 
-void guide() {
+void tester() {
     std::cout << "What function need to be called?\n"
         "1 - check_vector_constructors\n"
         "2 - check_vector_memory\n"
         "3 - check_vector_operators\n"
         "4 - check_vector_functions\n"
         "5 - stack-adapter\n"
-        "6 - end guide\n";
-    short select_function{};
-    std::cin >> select_function;
-    if (select_function >= 1 && select_function <= 6) {
-        switch (select_function) {
-        case(GUIDE::CHECK_VECTOR_CONSTRUCTORS):
+        "6 - all functions\n"
+        "7 - end guide\n";
+    short select_test_function{};
+    std::cin >> select_test_function;
+    if (select_test_function >= 1 && select_test_function <= 7) {
+        switch (select_test_function) {
+        case TESTER::CHECK_VECTOR_CONSTRUCTORS:
             std::cout << "You selected check_vector_constructors\n";
             check_vector_constructors();
             break;
-        case(GUIDE::CHECK_VECTOR_MEMORY):
+        case TESTER::CHECK_VECTOR_MEMORY:
             std::cout << "You selected check_vector_memory\n";
             check_vector_memory();
             break;
-        case(GUIDE::CHECK_VECTOR_OPERATORS):
+        case TESTER::CHECK_VECTOR_OPERATORS:
             std::cout << "You selected check_vector_operators\n";
             check_vector_operators();
             break;
-        case(GUIDE::CHECK_VECTOR_FUNCTIONS):
+        case TESTER::CHECK_VECTOR_FUNCTIONS:
             std::cout << "You selected check_vector_functions\n";
             check_vector_functions();
             break;
-        case(GUIDE::STACK_ADAPTER):
+        case TESTER::STACK_ADAPTER:
             std::cout << "You selected stack_adapter\n";
-            stack_adapter();
+            check_stack_adapter();
             break;
-        case(GUIDE::END_GUIDE):
+        case TESTER::ALL_FUNCTIONS:
+            std::cout << "You selected all functions\n";
+            check_vector_constructors();
+            check_vector_memory();
+            check_vector_operators();
+            check_vector_functions();
+            check_stack_adapter();
+            break;
+        case TESTER::END_GUIDE:
             std::cout << "You selected end guide\n";
             break;
         default:
