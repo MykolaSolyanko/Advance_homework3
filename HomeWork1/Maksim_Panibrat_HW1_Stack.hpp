61e0009a2e79753cecadd4d01943256b45ab253a
PanMaksim <60920533+PanMaksim@users.noreply.github.com>
2020-06-03 15:14:45 +0300

    Update Maksim_Panibrat_HW1_Stack.hpp

--- HomeWork1/Maksim_Panibrat_HW1_Stack.hpp
+++ HomeWork1/Maksim_Panibrat_HW1_Stack.hpp
@@ -1,62 +1,50 @@
 #pragma once
 
-using pair = std::pair<bool, const char*>;
+using pair = std::pair<bool, const char *>;
 
-template <typename T, class Container>
-class Stack_ad {
+template <typename T, class Container> class Stack_ad {
 public:
+  pair push(T value) {
+    if (vec.size_() > vec.capacity_()) {
+      return {false, "Stack full"};
+    }
+    vec.push_back(value);
+    return {true, "Push success"};
+  }
+  pair pop() {
+    if (vec.size_() == 0) {
+      return {false, "Stack clear"};
+    }
+    vec.erase(vec.end());
+    return {true, "Pop success"};
+  }
 
-	pair push(T value) {
-		if (vec.size_() > vec.capacity_()) {
-			return { false, "Stack full" };
-		}
-		vec.push_back(value);
-		return { true, "Push success" };
-	}
-	pair pop() {
-		if (vec.size_() == 0) {
-			return { false, "Stack clear" };
-		}
-		vec.erase(vec.end());
-		return { true, "Pop success" };
-	}
+  void clear() {
+    if (vec.size_() == 0) {
+      return;
+    }
+    vec.clear();
+  }
 
-	void clear() {
-		if (vec.size_() == 0) {
-			return;
-		}
-		vec.clear();
-	}
+  std::pair<bool, T> top() {
+    if (vec.size_() == 0) {
+      return {false, vec.back()};
+    }
 
-	std::pair<bool, T> top() {
-		if (vec.size_() == 0) {
-			return { false, vec.back() };
-		}
+    return {true, vec.back()};
+  }
 
-		return { true, vec.back() };
-	}
-	
-	template <typename ... Rest>
-	void emplace(Rest... rest) {
-		std::cout << __FUNCTION__ << std::endl;
-		vec.push_back(T{rest...});
-	}
+  template <typename... Rest> void emplace(Rest... rest) {
+    vec.push_back(T{rest...});
+  }
 
-	void resize(size_t new_size) {
-		std::cout << __FUNCTION__ << std::endl;
-		vec.resize(new_size);
-	}
+  void resize(size_t new_size) { vec.resize(new_size); }
 
-	const unsigned long long GetSize() {
-		return vec.size_();
-	}
-	const unsigned long long GetCapacity() {
-		return vec.capacity_();
-	}
-	void analize() {
-		vec.analize();
-	}
+  const unsigned long long GetSize() { return vec.size_(); }
+  const unsigned long long GetCapacity() { return vec.capacity_(); }
+  void analize() { vec.test_analize(); }
 
 private:
-	Container vec{};
+  Container vec{};
 };
+
